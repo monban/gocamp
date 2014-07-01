26908b3054899349ab4564da4df2645edb8176b0
Ian Flynn <himitsu.monban@gmail.com>
2014-07-01 00:42:58 -0400

    I really must get in the habbit of running gofmt before I commit. Also change gedit to use tabs.

--- world_test.go
+++ world_test.go
@@ -2,13 +2,13 @@ package gocamp
 
 import "testing"
 
-func TestCreateWorld(t *testing.T)	 {
-  w := &World{}
-  w.createWorld(3,3,1)
+func TestCreateWorld(t *testing.T) {
+	w := &World{}
+	w.createWorld(3, 3, 1)
 }
 
-func TestShowPlane(t *testing.T)	 {
-  w := &World{}
-  w.createWorld(3,3,1)
-  w.showTruePlane(1)
+func TestShowPlane(t *testing.T) {
+	w := &World{}
+	w.createWorld(3, 3, 1)
+	w.showTruePlane(1)
 }
