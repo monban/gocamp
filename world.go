f7b0936cc254d138d3102e36feef64909cb8e7fb
Ian Flynn <himitsu.monban@gmail.com>
2014-07-13 02:08:36 -0400

    coordinates managed by pt struct, basic maipulations

--- world.go
+++ world.go
@@ -66,15 +66,15 @@ type Pt struct {
 	z int
 }
 
-func (w *World) trueWidth() int {
+func (w *World) trueSizeX() int {
 	return w.size_x + 2
 }
 
-func (w *World) trueBreadth() int {
+func (w *World) trueSizeY() int {
 	return w.size_y + 2
 }
 
-func (w *World) trueDepth() int {
+func (w *World) trueSizeZ() int {
 	return w.size_z + 2
 }
 
@@ -85,29 +85,31 @@ func (self *World) dirOffset(direction dir) int {
 	case e:
 		return 1
 	case n:
-		return -(self.trueWidth())
+		return -(self.trueSizeX())
 	case s:
-		return -(self.trueWidth())
+		return -(self.trueSizeX())
 	case u:
-		return -(self.trueWidth() * self.trueBreadth())
+		return -(self.trueSizeX() * self.trueSizeY())
 	case d:
-		return self.trueWidth() * self.trueBreadth()
+		return self.trueSizeX() * self.trueSizeY()
 	}
 	return 0
 }
 
-func (w *World) createWorld(size_x int, size_y int, size_z int) {
+func createWorld(size_x int, size_y int, size_z int) World {
+	w := new(World)
 	w.size_x = size_x
 	w.size_y = size_y
 	w.size_z = size_z
 
-	numberBlocks := w.trueWidth() * w.trueBreadth() * w.trueDepth()
+	numberBlocks := w.trueSizeX() * w.trueSizeY() * w.trueSizeZ()
 
 	w.blocks = make([]Block, numberBlocks)
 
 	for i := 0; i < len(w.blocks); i++ {
 		w.blocks[i] = Block{void, void}
 	}
+	return *w
 }
 
 // Returns a slice containing the world at a certain z-level
@@ -115,17 +117,50 @@ func (w *World) getPlane(z_level int) (level []Block, err error) {
 	if z_level < 1 || z_level > w.size_z {
 		return nil, errors.New("no such z level")
 	}
-	begin := w.trueWidth() * w.trueDepth()
-	return w.blocks[begin : begin+w.dirOffset('d')], nil
+	begin := w.trueSizeX() * w.trueSizeZ()
+	return w.blocks[begin : begin+w.dirOffset(d)], nil
 }
 
 // Print out everything on a certain level, mostly for debugging
 func (w *World) showTruePlane(z_level int) {
 	plane, _ := w.getPlane(z_level)
-	for x := 0; x < w.trueWidth(); x++ {
-		for y := 0; y < w.trueBreadth(); y++ {
-			fmt.Printf(plane[(y*w.trueWidth())+x].ascii_representation())
+	for x := 0; x < w.trueSizeX(); x++ {
+		for y := 0; y < w.trueSizeY(); y++ {
+			fmt.Printf(plane[(y*w.trueSizeX())+x].ascii_representation())
 		}
 		fmt.Printf("\n")
+	}
+}
+
+// Converts point coordinates to an array index
+func (self *World) ptToIndex(point Pt) int {
+	// Skip the top border
+	i := self.trueSizeX()
+
+	// Skip the left padding and move to the correct column
+	i += point.x+1
+
+	// Move to the correct row
+	i += point.y*self.trueSizeX()
+	
+	// Move to the correct level
+	i += point.z*self.trueSizeX()*self.trueSizeY()
+	
+	return i
+}
+
+func (self *World) getBlock(pt Pt) Block {
+	return self.blocks[self.ptToIndex(pt)]
+}
+
+func (self *World) setBlock(pt Pt, blok Block) {
+	self.blocks[self.ptToIndex(pt)] = blok
+}
+
+func (self *World) fillPlane(z_level int, bulk material) {
+	for x := 0; x < self.size_x; x++ {
+		for y := 0; y < self.size_y; y++ {
+			self.setBlock(Pt{x,y,z_level}, Block{bulk, bulk})
+		}
 	}
 }
--- world_test.go
+++ world_test.go
@@ -2,17 +2,18 @@ package gocamp
 
 import "testing"
 
-func factoryWorld() *World {
-	w := &World{}
-	w.createWorld(3, 3, 1)
+func factoryWorld() World {
+	w := createWorld(3, 6, 1)
 	return w
 }
 
 func TestCreateWorld(t *testing.T) {
-	w := &World{}
-	w.createWorld(3, 3, 1)
+	w := createWorld(3, 3, 1)
+	if &w == nil {
+		t.Errorf("expected w to be a world")
+	}
 }
-
+/*
 func TestShowPlane(t *testing.T) {
 	w := factoryWorld()
 	w.showTruePlane(1)
@@ -23,4 +24,19 @@ func TestWestOffset(t *testing.T) {
 	w.showTruePlane(1)
 	// I don't know how to test these yet, perhaps I need a type representing a
 	// point on the map
+}
+*/
+func TestPtToIndex(t *testing.T) {
+	 // create a world with a true xyz size of 3x3x3
+	w := createWorld(1,1,1)
+	// create a point at the one valid space
+	point := Pt{0,0,0}
+	if w.ptToIndex(point) != 4 {
+		t.Errorf("expected 4 got %d", w.ptToIndex(point))
+	}
+}
+
+func TestSetBlock(t *testing.T) {
+	w := factoryWorld()
+	w.setBlock(Pt{1,1,1}, Block{rock,rock})
 }
