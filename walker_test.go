9401b73d98e4d5577ab04b4a8d17dff38d9defe5
Ian Flynn <himitsu.monban@gmail.com>
2014-08-11 00:13:49 -0400

    initialize Pt directly

--- walker_test.go
+++ walker_test.go
@@ -6,7 +6,7 @@ func TestWalker(t *testing.T) {
 	w := createDefaultWorld(6, 6, 1)
 	em := w.GetEntityManager()
 	walker := createWalkerEntity()
-	walker.SetDestination(NewPt(2, 2, 1))
+	walker.SetDestination(Pt{2, 2, 1})
 	e := Entitier(&walker)
 	(*em).Add(&e)
 	for i := 0; i < 3; i++ {
