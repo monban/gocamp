b1c953ecc2f2fd046d059b4e585273337a8bbfda
Ian Flynn <himitsu.monban@gmail.com>
2014-08-19 23:53:08 -0400

    bugs happen when you don't use your own encapsulation methods

--- walker_test.go
+++ walker_test.go
@@ -7,11 +7,9 @@ func TestWalker(t *testing.T) {
 	w.terrain.fillPlane(1, Block{air, rock})
 	w.terrain.NewLine(Block{rock, rock}, Pt{0, 1, 1}, 5, true)
 	w.terrain.NewLine(Block{rock, rock}, Pt{1, 3, 1}, 5, true)
-	em := w.EntityManager()
 	walker := createWalkerEntity()
 	walker.SetDestination(Pt{6, 6, 1})
-	em.Add(&walker)
-	walker.SetWorld(&w) // HACK BUG FIXME: WE SHOULD NOT HAVE TO DO THIS
+	w.AddEntity(&walker)
 	for walker.location != walker.destination {
 		w.Tick()
 		PrintRuneArray(w.GetEntireLevelAsRuneArray(1))
